8fb95ba227342e2e1e473913c15c32a579b2ed82
Pedro Vega <pedrovega468@yahoo.com>
2022-12-23 16:03:47 -0300

    Redirect403Personalizated

---
+++ src/main/java/com/MindHub/HomeBanking/configurations/AccessDeniedHandler.java
@@ -0,0 +1,20 @@
+package com.MindHub.HomeBanking.configurations;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        // redirigir al usuario a la página de acceso denegado personalizada
+        response.sendRedirect("/404.html");
+    }
+}
